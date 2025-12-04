2f89035ec95e2491aba472348f7ff155e2c46950
Verstand Tsai <hackertsaigenz@gmail.com>
2025-12-04 23:15:14 +0800

    Created Window and Texture class

---
+++ window.h
@@ -0,0 +1,44 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <SDL2/SDL.h>
+#include <functional>
+#include <vector>
+
+class Texture {
+    SDL_Texture *_texture;
+    int _width, _height;
+    SDL_Rect _rect;
+
+public:
+    Texture(int width, int height);
+    ~Texture();
+    int width() const;
+    int height() const;
+    void move(int x, int y);
+    void resize(int w, int h);
+    void update(std::function<void(int*)> callback);
+    friend class Window;
+};
+
+class Window {
+    static bool _initialized;
+    int _width, _height;
+    bool _closed;
+    SDL_Window *_window;
+    SDL_Renderer *_renderer;
+    std::vector<Texture> _textures;
+    SDL_Event _event;
+
+public:
+    Window(int width, int height);
+    ~Window();
+    int width() const;
+    int height() const;
+    bool closed() const;
+    void update();
+    Texture& create_texture(int width, int height);
+};
+
+#endif // WINDOW_H
+
